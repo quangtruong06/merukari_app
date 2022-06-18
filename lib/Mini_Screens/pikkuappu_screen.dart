4eeaa713725d2fa084894d930eff6c1a567d6195
quangtruong06 <106483026+quangtruong06@users.noreply.github.com>
2022-06-19 06:36:47 +0900

    new

--- lib/Mini_Screens/pikkuappu_screen.dart
+++ lib/Mini_Screens/pikkuappu_screen.dart
@@ -9,17 +9,20 @@ class Pikkuappu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    final double bigImg =160.0;
+    final double bigImg = 160.0;
     return Scrollbar(
       child: CustomScrollView(
         slivers: [
-          SliverToBoxAdapter(
+          SliverToBoxAdapter  (
             child: Container(
+              color: Colors.black12,
               padding: EdgeInsets.symmetric(vertical: defaultPadding),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Padding(
+                  Container(
+                    width: size.width,
+                    color: Colors.white,
                     padding: const EdgeInsets.all(defaultPadding),
                     child: Utils.customText(
                         text: "おすすめのピックアップ", fontWeight: FontWeight.bold),
@@ -27,7 +30,7 @@ class Pikkuappu extends StatelessWidget {
                   Container(
                     padding: EdgeInsets.all(16.0),
                     width: size.width,
-                    height: 275,
+                    height: size.height / 2.9,
                     decoration: const BoxDecoration(
                         gradient: LinearGradient(
                       begin: Alignment.topLeft,
@@ -38,73 +41,138 @@ class Pikkuappu extends StatelessWidget {
                       ],
                       colors: [Color(0xFFffd0db), Color(0xFF66D7EB)],
                     )),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    child: Wrap(children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Utils.customText(
+                                    text: "夏素材のバッグで季節を先取りしよう",
+                                    fontWeight: FontWeight.bold),
+                                Icon(Icons.navigate_next)
+                              ]),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Utils.customText(
+                              text:
+                                  "夏になると欲しくなるかごバッグやメッシュ、麻、キャンバス、クリアバッグなど涼しげな素材のバッグを集めました。",
+                              size: 10.0),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Row(
                             children: [
-                              Utils.customText(
-                                  text: "夏素材のバッグで季節を先取りしよう",
-                                  fontWeight: FontWeight.bold),
-                              Icon(Icons.navigate_next)
-                            ]),
-                        SizedBox(height: 10,),
-                        Utils.customText(text:
-                          "夏になると欲しくなるかごバッグやメッシュ、麻、キャンバス、クリアバッグなど涼しげな素材のバッグを集めました。",size: 10.0
-                        ),
-                        SizedBox(height: 20,),
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              Container(
-                                  width: size.width/2 - 16,
-                                  child: imageItem(imgSize: bigImg, itemValue: 2500)),
-                              Container(
-                                width: size.width/2 - 16,
+                              Expanded(
+                                  child: imageItem(
+                                      imgSize:
+                                          size.width / 2 - defaultPadding * 3.5,
+                                      itemValue: 2500)),
+                              Expanded(
                                 child: Wrap(
-                                  spacing: 6,
-                                  runSpacing: 6,
+                                  direction: Axis.horizontal,
+                                  spacing: 3,
+                                  runSpacing: 3,
                                   children: [
                                     Container(
-                                      width: size.width/4 - 13,
-                                        child: imageItem(imgSize: bigImg, itemValue: 2500)),
+                                        child: imageItem(
+                                            imgSize: size.width / 4 -
+                                                defaultPadding * 2,
+                                            itemValue: 2500)),
                                     Container(
-                                        width: size.width/4 - 13,
-                                        child: imageItem(imgSize: bigImg, itemValue: 2500)),
+                                        child: imageItem(
+                                            imgSize: size.width / 4 -
+                                                defaultPadding * 2,
+                                            itemValue: 2500)),
                                     Container(
-                                        width: size.width/4 - 13,
-                                        child: imageItem(imgSize: bigImg, itemValue: 2500)),
+                                        child: imageItem(
+                                            imgSize: size.width / 4 -
+                                                defaultPadding * 2,
+                                            itemValue: 2500)),
                                     Container(
-                                        width: size.width/4 - 13,
-                                        child: imageItem(imgSize: bigImg, itemValue: 2500)),
-
+                                        child: imageItem(
+                                            imgSize: size.width / 4 -
+                                                defaultPadding * 2,
+                                            itemValue: 2500)),
                                   ],
                                 ),
                               )
-                              // gridViewer()
                             ],
-                        )
-                      ],
-                    ),
+                          ),
+                        ],
+                      ),
+                    ]),
                   )
                 ],
               ),
             ),
           ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.all(defaultPadding),
+              child: Utils.customText(
+                  text: "おすすめのピックアップ一覧", fontWeight: FontWeight.bold),
+            ),
+          ),
+          SliverList(
+              delegate: SliverChildBuilderDelegate((context, index) {
+            return Container(
+              padding: EdgeInsets.all(defaultPadding),
+              decoration: BoxDecoration(
+                  border:
+                      Border(top: BorderSide(width: 1, color: Colors.black12))),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Utils.customText(
+                      text: "ナイトルーティンで素敵な夜を。人気の美顔器5選",
+                      fontWeight: FontWeight.bold,
+                      size: 13.0),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Wrap(
+                    direction: Axis.horizontal,
+                    spacing: 4,
+                    children: [
+                      imageItem(
+                          imgSize: size.width / 4 - defaultPadding,
+                          itemValue: 2500),
+                      imageItem(
+                          imgSize: size.width / 4 - defaultPadding,
+                          itemValue: 2500),
+                      imageItem(
+                          imgSize: size.width / 4 - defaultPadding,
+                          itemValue: 2500),
+                      imageItem(
+                          imgSize: size.width / 4 - defaultPadding,
+                          itemValue: 2500),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Column(
+                    children: [
+                      Utils.customText(
+                          text:
+                              "毛穴やたるみなどのスキンケアや、ほうれい線、リフトアップをセルフケアしたい！　美容クリニックで使われているプロ仕様のアイテムもメルカリで販売中です。（集計期間：2021年5月〜2022年4月）",
+                          size: 10.5),
+                      Align(
+                        alignment: Alignment.bottomRight,
+                        child: Utils.customText(
+                            text: "すべて見る", size: 11.0, color: Colors.blue),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            );
+          }, childCount: 8)),
         ],
       ),
     );
   }
-}
-class gridViewer extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return GridView.count(crossAxisCount: 2,
-    children: List.generate(4, (index) {
-      return imageItem(imgSize: defaultImageSize/2, itemValue: 2500);
-        }),);
-  }
-
 }
